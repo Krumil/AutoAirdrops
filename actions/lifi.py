95ffaea76072c9edd2a980870252b17589df907c
s.saletti <s.saletti@qzrstudio.com>
2024-02-14 23:14:07 +0100

    push

--- actions/lifi.py
+++ actions/lifi.py
@@ -1,5 +1,5 @@
 import requests
-from utils import load_files, init_web3, estimate_gas_limit, get_chain_key, convert_eth_to_wei
+from utils import load_files, init_web3, estimate_gas_limit, get_chain_key, get_chain_id, convert_eth_to_wei, get_token_balance
 
 # Load necessary data
 _, _, _, alchemy_url_list, _, _, erc20_abi, lifi_chains = load_files()
@@ -33,7 +33,7 @@ def get_status(bridge, starting_chain, destination_chain, tx_hash):
 	response = requests.get(url, params=params)
 	return response.json()
 
-def check_and_set_allowance(web3, account_address, private_key, token_address, approval_address, amount, abi):
+def check_and_set_allowance(web3, chain, account_address, private_key, token_address, approval_address, amount, abi):
 	zero_address ='0x0000000000000000000000000000000000000000'
 	if token_address == zero_address:
 		return
@@ -42,10 +42,11 @@ def check_and_set_allowance(web3, account_address, private_key, token_address, a
 	current_allowance = contract.functions.allowance(account_address, approval_address).call()
 
 	if current_allowance < amount:
+		chain_id = get_chain_id(chain)
 		nonce = web3.eth.get_transaction_count(account_address)
-		approve_tx = contract.functions.approve(approval_address, amount).buildTransaction({
+		approve_tx = contract.functions.approve(approval_address, amount).build_transaction({
 			'from': account_address,
-			'chainId': web3.eth.chainId,
+			'chainId': chain_id,
 			'nonce': nonce,
 		})
 		signed_tx = web3.eth.account.sign_transaction(approve_tx, private_key)
@@ -57,7 +58,7 @@ def execute_swap(starting_chain, destination_chain, from_token, to_token, from_a
 	web3 = init_web3(alchemy_url)	
 	quote = get_quote(starting_chain, destination_chain, from_token, to_token, from_amount, account_address)
 	
-	check_and_set_allowance(web3, account_address, private_key, quote['action']['fromToken']['address'], quote['estimate']['approvalAddress'], from_amount, erc20_abi)
+	check_and_set_allowance(web3, starting_chain, account_address, private_key, quote['action']['fromToken']['address'], quote['estimate']['approvalAddress'], from_amount, erc20_abi)
 	
 	nonce = web3.eth.get_transaction_count(account_address)
 
@@ -85,3 +86,13 @@ def split_send_eth(starting_chain, destination_chains, from_token, to_token, fro
 	for chain in destination_chains:
 		execute_swap(starting_chain, chain, from_token, to_token, split_amount, account_address, private_key)
 	
+
+# create a function to execute the swap within the same chain (e.g. Ethereum to Ethereum) with ETH as the from token and USDC as the to token
+def execute_swap_ETH_to_USDC(chain, account_address, private_key, amount):
+	receipt = execute_swap(chain, chain, 'ETH', 'USDC', amount, account_address, private_key)
+	return receipt
+
+def execute_swap_all_USDC_to_ETH(chain, account_address, private_key) :
+	amount = get_token_balance(chain, account_address, 'USDC')
+	receipt = execute_swap(chain, chain, 'USDC', 'ETH', amount, account_address, private_key)
+	return receipt
--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 from actions.merkly import execute_merkly_transaction
-from actions.lifi import execute_swap, split_send_eth
+from actions.lifi import execute_swap, split_send_eth, execute_swap_ETH_to_USDC, execute_swap_all_USDC_to_ETH
 from utils import randomize_transaction_parameters, convert_eth_to_wei, get_token_balance
+import random
 
 starting_chain, destination_chain, account_address, private_key = randomize_transaction_parameters()
 # starting_chain = "zksync_era"
-starting_chain = "arbitrum"
+# starting_chain = "arbitrum"
 # destination_chains = ["arbitrum","optimism"]
 # print(f"Starting chain: {starting_chain}")
 # print(f"Destination chain: {destination_chain}")
 # print(f"Account address: {account_address}")
 
-min_eth = 0.04
 
+# min_eth = 0.02
 # execute_merkly_transaction(nft=True, amount=1, starting_chain=starting_chain, destination_chain=destination_chain, account_address=account_address, private_key=private_key)
 # execute_swap(starting_chain, destination_chain, "ETH", "ETH", convert_eth_to_wei(min_eth), account_address, private_key)
 # split_send_eth(starting_chain, destination_chains, "ETH", "ETH", min_eth, account_address, private_key)
+# execute_swap_ETH_to_USDC(starting_chain,account_address, private_key, convert_eth_to_wei(min_eth))
+# execute_swap_all_USDC_to_ETH(starting_chain, account_address, private_key)
 
-balance = get_token_balance(starting_chain, account_address, "WETH")
-print(balance)
+
+def random_swap_amount():
+    return random.uniform(0.02, 0.04)  # Randomly select an amount between 0.01 and 0.1 ETH for swapping
+
+def execute_random_swaps_on_path(chains, account_address, private_key):
+    for chain in chains:
+        amount_eth_to_usdc = random_swap_amount()  # Random amount for ETH to USDC swap
+        amount_in_wei = convert_eth_to_wei(amount_eth_to_usdc)
+        eth_to_usdc_receipt = execute_swap(chain, chain, 'ETH', 'USDC', amount_in_wei, account_address, private_key)
+        print(f"Swapped {amount_eth_to_usdc} ETH to USDC on {chain}")
+
+        # Wait for the swap to be completed and the balance to update
+        # This step is simplified; in a real scenario, you should check the updated balance
+
+        usdc_to_eth_receipt = execute_swap(chain, chain, 'USDC', 'ETH', amount_in_wei, account_address, private_key)
+        print(f"Swapped back to ETH on {chain}")
+        
+chains = ['arbitrum', 'optimism', 'zksync_era', 'scroll']
+execute_random_swaps_on_path(chains, account_address, private_key)
