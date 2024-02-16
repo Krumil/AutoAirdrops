ae00b4ddb11347412f7104fb1b238987919e2d7f
s.saletti <s.saletti@qzrstudio.com>
2024-02-16 10:22:14 +0100

    push

--- app.py
+++ app.py
@@ -23,8 +23,12 @@ starting_chain, destination_chain, account_address, private_key = randomize_tran
 
 def random_swap_amount(chain, account_address):
 	eth_balance = get_token_balance(chain, account_address, "ETH")
-	max_eth_to_swap = eth_balance * 0.8
-	min_eth_to_swap = eth_balance * 0.2
+	max_eth_to_swap = eth_balance * 0.9
+	min_eth_to_swap = convert_eth_to_wei(0.02)
+
+	if max_eth_to_swap < min_eth_to_swap:
+		return False
+
 	max_eth_to_swap = int(max_eth_to_swap)
 	min_eth_to_swap = int(min_eth_to_swap)
 	amount_in_wei = random.randint(min_eth_to_swap, max_eth_to_swap)
@@ -33,6 +37,10 @@ def random_swap_amount(chain, account_address):
 def execute_random_swaps_on_path(chains, account_address, private_key):
 	for chain in chains:
 		amount_in_wei = random_swap_amount(chain, account_address)  
+
+		if not amount_in_wei:
+			continue
+
 		amount_eth_to_usdc = convert_wei_to_eth(amount_in_wei)
 		print(f"Swapping {amount_eth_to_usdc} ETH to USDC on {chain}")
 		execute_swap_ETH_to_USDC(chain,account_address, private_key, amount_in_wei)
@@ -40,13 +48,6 @@ def execute_random_swaps_on_path(chains, account_address, private_key):
 		execute_swap_all_USDC_to_ETH(chain, account_address, private_key)
 		print(f"Swapped back to ETH on {chain}")
 		if chain != chains[-1]:
-			eth_balance_in_wei = get_token_balance(chain, account_address, "ETH")
-			eth_balance = convert_wei_to_eth(eth_balance_in_wei)
-			if eth_balance < 0.02:
-				print(f"Not enough ETH to bridge to {chains[chains.index(chain) + 1]}")
-				continue
-
-
 			next_chain = chains[chains.index(chain) + 1]
 			print(f"Bridging {amount_eth_to_usdc} ETH to {next_chain}")
 			execute_swap(chain, next_chain, "ETH", "ETH", amount_in_wei, account_address, private_key)
